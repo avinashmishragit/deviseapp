8cb78a83b00a5a524fee9258c348e630c4e69929
innocentavi <mishra.avinash35@gmail.com>
2012-10-30 12:41:23 +0530

    devise and devise genertor added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deviseapp::Application.config.secret_token = '7648c08cffebe93f62f18bea6d9af82ffe834d8dd007256655da6a92f27f25159f12b241da01bb18384d8a61a8f55ddeb2f1429965ec23a8ffeac6f52fff8243'
